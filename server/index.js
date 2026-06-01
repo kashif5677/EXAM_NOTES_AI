2f99d86fc43aa706ecdd089ab4adc78112bdb088
Md Kashif Nisar <mdkashifnisar5677@gmail.com>
2026-06-01 13:22:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://exam-notes-ai-client-o2o0.onrender.com',
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     }
